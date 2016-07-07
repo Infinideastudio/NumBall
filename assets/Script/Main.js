458806b6b3368f3ea18679bbb99dcc8369ca76f7
Null <abc612008@126.com>
2016-07-07 16:54:15 +0800

    Improve
    更新:
    彩色的球
    更合理的大小
    高清材质
    优化甩的手感
    修复Bugs

--- assets/Script/Main.js
+++ assets/Script/Main.js
@@ -5,6 +5,10 @@ function posDiff(x1,y1,x2,y2){
     return d;
 }
 
+function calcSize(num){
+    return 1.5-50/(num+33);
+}
+
 var balls = [];
 
 var TimeEachRound=5;
@@ -33,8 +37,8 @@ var Ball = cc.Class({
     ortimer:0,
     ballnode : null,
     labelnode: null,
-    pnode: null,
-    psf:null,
+    pnode: null,psf:null,
+    tow:false,toa:false,tod:false,tos:false,
     ctor: function (node,sf) {
         this.pnode=node;
         this.psf=sf;
@@ -51,7 +55,8 @@ var Ball = cc.Class({
         sp.spriteFrame = sf;
         this.ballnode.parent = node;
         this.ballnode.setPosition(this.x,this.y);
-        this.ballnode.scale=this.num/30;
+        this.ballnode.scale=calcSize(this.num);
+        this.ballnode.color=new cc.Color(Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256));
         
         //Label
         this.labelnode = new cc.Node('balllabel');
@@ -60,12 +65,26 @@ var Ball = cc.Class({
         this.labelnode.parent = node;
         this.labelnode.setPosition(this.x,this.y);
         
+        this.ballnode.on(cc.Node.EventType.TOUCH_START, function ( event ) {
+            this.orx=this.x;
+            this.ory=this.y;
+            this.ortimer=timer;
+            this.tow=false;
+            this.toa=false;
+            this.tod=false;
+            this.tos=false;
+        }.bind(this));
+        
         //Move
-        this.ballnode.on(cc.Node.EventType.TOUCH_MOVE , function ( event ) {
+        this.ballnode.on(cc.Node.EventType.TOUCH_MOVE, function ( event ) {
             var size = cc.winSize;
             this.setPos(event.getLocationX()-size.width/2,event.getLocationY()-size.height/2);
-            //Fly
-            if(this.num>=5&&timer-this.ortimer<2&&posDiff(this.x,this.y,this.orx,this.ory)>100){
+            if(this.x-this.orx>50) this.tow=true;
+            if(this.x-this.orx<-50) this.tos=true;
+            if(this.y-this.ory>50) this.tod=true;
+            if(this.y-this.ory<-50)this.toa=true;
+            //Throw
+            if(this.num>=5&&(this.tow&&this.tos)||(this.tod&&this.toa)){
                 let ballNum=Math.round(Math.random()*8)+2;
                 let allNum=this.num;
                 for(let i=0;i<ballNum;i++){
@@ -90,7 +109,7 @@ var Ball = cc.Class({
             }
         }.bind(this));
         
-        //Split
+
         this.ballnode.on(cc.Node.EventType.TOUCH_END, function ( event ) {
             //Hit check
             for(let i=0;i<balls.length;i++){
@@ -114,6 +133,7 @@ var Ball = cc.Class({
                 }
             }
             
+            //Split
             if(this.num>=2&&timer-this.ortimer<1&&posDiff(this.x,this.y,this.orx,this.ory)<10){
                 let ball1=new Ball(this.pnode, this.psf);
                 let ball2=new Ball(this.pnode, this.psf);
@@ -130,9 +150,6 @@ var Ball = cc.Class({
                 balls.push(ball1);
                 balls.push(ball2);
             }
-            this.orx=this.x;
-            this.ory=this.y;
-            this.ortimer=timer;
         }.bind(this));
     },
     setPos: function(x,y){
@@ -146,7 +163,7 @@ var Ball = cc.Class({
     setNum: function(num){
         this.num=num;
         this.labelnode.getComponent(cc.Label).string=this.num.toString();
-        this.ballnode.scale=this.num/30;
+        this.ballnode.scale=calcSize(this.num);
     },
     rm: function(){
         this.ballnode.removeFromParent();
@@ -199,22 +216,32 @@ cc.Class({
         
         if(timer>=TimeEachRound){
             timer=0;
-            for(let i=0;i<balls.length;i++){
+            for(let i=0;i<balls.length;){
                 //apply formula
                 balls[i].setNum(parseInt(eval(balls[i].num.toString()+formula)));
                 if(balls[i].num<=0){
                     balls[i].rm();
                     balls=balls.slice(0,i).concat(balls.slice(i+1,balls.length));
+                }else{
+                    i++;
                 }
             }
             if(balls.length==0){
                 gameover=true;
                 this.formulaNode.string="Game over!";
                 this.scoreNode.string="Score: " + score;
+                this.timerNode.string="";
                 return;
             }
-            formula=formulas[Math.floor(Math.random()*formulas.length)].replace("n", Math.floor(Math.random()*9)+1);
+            formula=formulas[Math.floor(Math.random()*formulas.length)].replace("n", Math.floor(Math.random()*9)+2);
             this.formulaNode.string=formula;
+        }
+        var size = cc.winSize;
+        for(let i=0;i<balls.length;i++){
+            if(balls[i].x>size.width/2) balls[i].setPos(size.width/2,balls[i].y);
+            if(balls[i].x<-size.width/2) balls[i].setPos(-size.width/2,balls[i].y);
+            if(balls[i].y>size.height/2) balls[i].setPos(balls[i].x,size.height/2);
+            if(balls[i].y<-size.height/2) balls[i].setPos(balls[i].x,-size.height/2);
         }
     },
 });
