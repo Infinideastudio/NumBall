4aaa91cbe2071add7ec1347be39eaec2383d1140
Null <abc612008@126.com>
2016-07-07 15:03:20 +0800

    公式

--- assets/Script/Main.js
+++ assets/Script/Main.js
@@ -11,6 +11,16 @@ var TimeEachRound=10;
 
 var timer=0;
 
+var score=0;
+
+var formulas=[
+    "+n",
+    "-n",
+    "*n",
+    "/n"
+    ];
+
+var formula = "";
 var Ball = cc.Class({
     name: "Ball",
     num: 0,
@@ -48,21 +58,45 @@ var Ball = cc.Class({
         this.labelnode.parent = node;
         this.labelnode.setPosition(this.x,this.y);
         
-        //移动
+        //Move
         this.ballnode.on(cc.Node.EventType.TOUCH_MOVE , function ( event ) {
             var size = cc.winSize;
             this.setPos(event.getLocationX()-size.width/2,event.getLocationY()-size.height/2);
+            //Fly
+            if(this.num>=5&&timer-this.ortimer<2&&posDiff(this.x,this.y,this.orx,this.ory)>100){
+                let ballNum=Math.round(Math.random()*8)+2;
+                let allNum=this.num;
+                for(let i=0;i<ballNum;i++){
+                    let xd=Math.round(Math.random()*38)+12;
+                    let yd=Math.round(Math.random()*38)+12;
+                    if(Math.random()>0.5) xd=-xd;
+                    if(Math.random()>0.5) yd=-yd;
+                    let takeNum=i!=ballNum-1?Math.floor(Math.random()*allNum):allNum;
+                    if(takeNum===0) break;
+                    let ball=new Ball(this.pnode, this.psf);
+                    ball.setPos(this.x + xd,this.y +yd);
+                    ball.setNum(takeNum);
+                    allNum-=takeNum;
+                    balls.push(ball);
+                }
+                this.rm();
+                let id=0;
+                for(id=0;id<balls.length;id++){
+                    if(balls[id].x==this.x&&balls[id].y==this.y) break;
+                }
+                balls=balls.slice(0,id).concat(balls.slice(id+1,balls.length));
+            }
         }.bind(this));
         
-        //平分
+        //Split
         this.ballnode.on(cc.Node.EventType.TOUCH_END, function ( event ) {
-            //碰撞检测
-            for(var i=0;i<balls.length;i++){
-                var balli=balls[i];
-                var ballj=this;
-                if(balli.x==ballj.x&&balli.y==ballj.y) continue; //自己不和自己比较
-                if(posDiff(balli.x,balli.y,ballj.x,ballj.y)<20){ //距离小于20即合并
-                    var ball=new Ball(this.pnode, this.psf);
+            //Hit check
+            for(let i=0;i<balls.length;i++){
+                let balli=balls[i];
+                let ballj=this;
+                if(balli.x==ballj.x&&balli.y==ballj.y) continue; 
+                if(posDiff(balli.x,balli.y,ballj.x,ballj.y)<20){ 
+                    let ball=new Ball(this.pnode, this.psf);
                     ball.setPos(ballj.x,ballj.y);
                     ball.setNum(balli.num+ballj.num);
                     balli.rm();
@@ -77,9 +111,10 @@ var Ball = cc.Class({
                     break;
                 }
             }
+            
             if(this.num>=2&&timer-this.ortimer<1&&posDiff(this.x,this.y,this.orx,this.ory)<10){
-                var ball1=new Ball(this.pnode, this.psf);
-                var ball2=new Ball(this.pnode, this.psf);
+                let ball1=new Ball(this.pnode, this.psf);
+                let ball2=new Ball(this.pnode, this.psf);
                 ball1.setPos(this.x - 15,this.y);
                 ball1.setNum(Math.floor(this.num/2));
                 ball2.setPos(this.x + 15,this.y);
@@ -128,28 +163,49 @@ cc.Class({
         timerNode: {
             default: null,
             type: cc.Label
-        }
+        },
+        formulaNode: {
+            default: null,
+            type: cc.Label
+        },
+        scoreNode:{
+            default: null,
+            type: cc.Label
+        },
     },
     
     // use this for initialization
     onLoad: function () {
         for(var i=0;i<Math.round(Math.random()*10);i++)
             balls.push(new Ball(this.node, this.ballsprite));
+        
+        formula=formulas[Math.floor(Math.random()*formulas.length)].replace("n", Math.floor(Math.random()*10));
+        this.formulaNode.string=formula;
     },
 
     // called every frame
     update: function (dt) {
         timer+=dt;
         this.timerNode.string="Timer: "+(TimeEachRound-Math.ceil(timer)).toString();
+        var sumnum=0;
+        for(let i=0;i<balls.length;i++){
+            sumnum+=balls[i].num;
+        }
+        if(sumnum>score) score=sumnum;
+        this.scoreNode.string="Score: " + sumnum + "/" + score;
+        
         if(timer>=TimeEachRound){
             timer=0;
-            for(var i=0;i<balls.length;i++){
-                var x=balls[i].num;
-                //应用公式
-                var y=x;
-                
-                balls[i].setNum(y);
+            for(let i=0;i<balls.length;i++){
+                //apply formula
+                balls[i].setNum(parseInt(eval(balls[i].num.toString()+formula)));
+                if(balls[i].num==0){
+                    balls[i].rm();
+                    balls=balls.slice(0,i).concat(balls.slice(i+1,balls.length));
+                }
             }
+            formula=formulas[Math.floor(Math.random()*formulas.length)].replace("n", Math.floor(Math.random()*10));
+            this.formulaNode.string=formula;
         }
     },
 });
